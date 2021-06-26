c711809a44b880f818c00af39273d14dbfe656dc
Zaida Guadalupe <zaida@secret-source.eu>
2021-06-26 19:28:35 +0100

    Add livewire component for form

---
+++ app/Http/Livewire/TireBatchForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TireBatchForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.tire-batch-form');
+    }
+}
