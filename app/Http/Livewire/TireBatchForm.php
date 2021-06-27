1307389c2c69f82017a9354d3b849c9c284aec7d
Zaida Guadalupe <zaida@secret-source.eu>
2021-06-27 08:53:00 +0100

    Add tire form

--- app/Http/Livewire/TireBatchForm.php
+++ app/Http/Livewire/TireBatchForm.php
@@ -6,8 +6,33 @@ use Livewire\Component;
 
 class TireBatchForm extends Component
 {
+    public $identifier;
+    public $batch_date;
+    public $country;
+    public $rubber_percentage;
+    public $black_carbon_percentage;
+    public $fibre_percentage;
+    public $quality;
+
     public function render()
     {
         return view('livewire.tire-batch-form');
+    }
+
+    public function submit()
+    {
+            $validatedData = $this->validate([
+                'identifier'                => 'required|min:6',
+                'batch_date'                => 'required|date',
+                'country'                   => 'required',
+                'rubber_percentage'         => 'required', 
+                'black_carbon_percentage'   => 'required',
+                'fibre_percentage'          => 'required',
+                'quality'                   => 'required'
+            ]);
+  
+        
+  
+        return redirect()->to('/estimation');
     }
 }
--- resources/views/livewire/tire-batch-form.blade.php
+++ resources/views/livewire/tire-batch-form.blade.php
@@ -1,3 +1,63 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div class="py-12 ">
+    <div class="mt-8 max-w-md">
+    <div clas="grid grid-cols-1 gap-6">
+    
+
+    <form wire:submit.prevent="submit">
+        <div class="px-4  py-3">
+            <label class="block">
+                <span class="text-gray-700"> {{ __('Identifier')}} </span>
+                <input type="text" class="form-input mt-1 block " id="identifier" placeholder="Enter batch identifier" wire:model="identifier">
+            </label>
+        </div>
+
+        <div class="px-4  py-3">
+            <label class="block">
+                <span class="text-gray-700"> {{ __('Batch Date')}} </span>
+                <input type="date" class="form-input mt-1 block w-full"  id="batch_date" placeholder="Enter batch date">
+            </label>
+        </div>
+    
+    
+
+        <div class="px-4 py-3">
+            <label class="block">
+                <span class="text-gray-700"> {{ __('Country')}} </span>
+                <input type="text" class="form-input mt-1 block w-full"  id="country"  placeholder="Enter country">
+            </label>
+        </div>
+
+        <div class="px-4 py-3">
+            <label class="block">
+                <span class="text-gray-700">{{ __('Percentage of rubber')}}</span>
+                <input type="range" class="mt-1 block w-full" id="rubber_percentage">
+            </label>
+        </div>   
+        <div class="px-4 py-3">
+            <label class="block">
+                <span class="text-gray-700">{{ __('Percentage of black carbon')}}</span>
+                <input type="range" class="mt-1 block w-full" id="black_carbon_percentage">
+            </label>
+        </div>  
+        <div class="px-4 py-3">
+            <label class="block">
+                <span class="text-gray-700">{{ __('Percentage of fibre')}}</span>
+                <input type="range" class="mt-1 block w-full" id="fibre_percentage">
+            </label>
+        </div>  
+        
+
+        <div class="px-4 py-3">
+            <label class="block">
+                <span class="text-gray-700">Quality (range 0-99)</span>
+                <input type="range" class="mt-1 block w-full" id="quality">
+            </label>
+        </div>   
+    
+        <div class="px-4 py-3">
+            <button type="submit" class="btn btn-primary">{{ _('Get Estimation')}}</button>
+            </div>
+    </form>
+    </div>
+    </div>
 </div>
