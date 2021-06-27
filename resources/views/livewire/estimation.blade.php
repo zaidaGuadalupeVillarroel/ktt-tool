254e5bfa5fb31e378d25033fc98d349a1de6271d
Zaida Guadalupe <zaida@secret-source.eu>
2021-06-27 08:53:41 +0100

    Add results page

---
+++ resources/views/livewire/estimation.blade.php
@@ -0,0 +1,48 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    
+
+    <div class="mt-8 text-2xl">
+        Welcome to your estimation application!
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        KTT has saved the data related to your inputs for future searchs in database, the generated identifier for the estimation is : <b>HNZXFGNLG1F</b>
+    </div>
+</div>
+
+
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+    <div class="p-6">
+        <div class="flex items-center">
+            
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Best End product</div>
+        </div>
+
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+                The best match acording to your inputs is .. 
+                <p><b> Acoustic screen</b></p>
+                
+            </div>
+
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
+        <div class="flex items-center">
+            
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Best Partner</div>
+        </div>
+
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+                The best match acording to your inputs is ..
+                <h3><b> Ute Pantallas Acusticas, Calle San Severo, 18, 28042, Madrid, Madrid</b></h3>
+                
+            </div>
+
+            
+        </div>
+    </div>
+
+</div>
